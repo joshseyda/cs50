2579b885c44362889226bf656e7b10c360401583
joshseyda <josh.seyda@gmail.com>
2019-09-26 10:38:18 -0400

    more compiling instructions

--- 1/hello.c
+++ 1/hello.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
-// clang hello.c to run
+// clang hello.c to run compiler
+// ./a.out will run the compiled code
+// clang -o hello hello.c enables you to run ./hello instead of a.out
+// make hello is a less verbose way to do the line above
 
 int main(void)
 {
