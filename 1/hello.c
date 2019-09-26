4a90eb4d1965e5f7d369e7bbbd6635aff18f4ede
joshseyda <josh.seyda@gmail.com>
2019-09-26 13:10:48 -0400

    setting up cs50 C library

--- 1/hello.c
+++ 1/hello.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
+#include <cs50.h>
+
 // clang hello.c to run compiler
 // ./a.out will run the compiled code
 // clang -o hello hello.c enables you to run ./hello instead of a.out
 // make hello is a less verbose way to do the line above
+// gcc -g hello.c -o hello -lcs50 -lm  <- needed this command to run with cs50 library properly
 
 int main(void)
 {
-  printf("hello, world\n");
+  string name = get_string("What is your name>\n");
+  printf("hello, %s\n", name);
 }
