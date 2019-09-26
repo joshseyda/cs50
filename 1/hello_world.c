c8ec8f5f108fe9272389ff77f08950cb79012313
joshseyda <josh.seyda@gmail.com>
2019-09-26 10:06:17 -0400

    first tools in C

--- 1/hello_world.c
+++ 1/hello_world.c
@@ -1,3 +1,24 @@
-printf("hello, world\n");
-int counter = 0;
-counter =+ 1;
+printf("hello, world\n"); // \n is to start a new line after
+
+int counter = 0; // instantiate a variable, it needs to be typed
+counter ++; // increment variable by 1
+
+printf(counter\n);
+
+int x = 3;
+int y = 8
+
+// basic pattern of conditional statements
+if (x < y)
+{
+  printf("x is less than y\n");
+}
+else if (x > y)
+{
+  printf("x is greater than y\n");
+}
+else if (x == y)
+{
+  printf("x is equal to y\n");
+}
+
