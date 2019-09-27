5533af9c2fd6df5e7091b8c0c156d9357af825f1
joshseyda <josh.seyda@gmail.com>
2019-09-27 16:12:59 -0400

    cleaned up scores2 to be marginally less redundant

--- 2/scores2.c
+++ 2/scores2.c
@@ -1,25 +1,28 @@
 #include <stdio.h>
 #include <cs50.h>
 
+const int COUNT = 3;
+void chart(int score);
 
 int main(void)
 {
     // get scores from user and push them to a declared array of type int
-    int scores[3];
-    for (int i = 0; i < 3; i++)
+    int scores[COUNT];
+    for (int i = 0; i < COUNT; i++)
     {
         scores[i] = get_int("Score %i: ", i + 1);
     }
     
     // Chart scores
-    for (int i = 0; i < 3; i++)
+    for (int i = 0; i < COUNT; i++)
     {
         printf("Score %i: ", i + 1);
         chart(scores[i]);
     }
+}
 
 void chart(int score)
-{
+{    
     for (int i = 0; i < score; i++)
     {
         printf("#");
