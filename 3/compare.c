9c42dd9b4ff9f2c33cef8aac3c3fc033997e83ad
joshseyda <josh.seyda@gmail.com>
2019-10-01 22:13:32 -0400

    ditch comparison method for out of the box strcmp function

--- 3/compare.c
+++ 3/compare.c
@@ -2,14 +2,13 @@
 #include <stdio.h>
 #include <string.h>
 
-bool compare_strings( char *a, char *b);
 
 int main(void)
 {
     char *s = get_string("s: ");
     char *t = get_string("t: ");
 
-    if(compare_strings(s, t))
+    if(strcmp(s, t) == 0)
     {
         printf("same\n");
     }
@@ -17,20 +16,4 @@ int main(void)
     {
         printf("different\n");
     }
-}
-
-bool compare_strings( char *a, char *b)
-{
-    if (strlen(a) != strlen(b))
-    {
-        return false;
-    }
-    for (int i = 0, n = strlen(a); i < n; i++)
-    {
-        if (a[i] != b[i])
-        {
-            return false;
-        }
-    }
-    return true;
 }
