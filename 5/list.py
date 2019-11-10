203e47b0a892c586f7f82ce6499e6c3eb658d1e6
joshseyda <josh.seyda@gmail.com>
2019-11-10 02:59:57 -0500

    evaluating numbers in list

---
+++ 5/list.py
@@ -0,0 +1,16 @@
+from cs50 import get_int
+
+numbers = []
+
+while True:
+    number = get_int("Number: ")
+
+    if not number:
+        break
+    
+    if number not in numbers:
+        numbers.append(number)
+
+for number in numbers:
+    print(number)
+
